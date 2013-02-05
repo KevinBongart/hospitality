3a060463a8719468edbde245603479f0e47a53bf
Kevin Bongart <contact@kevinbongart.net>
2013-02-05 18:44:27 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hospitality::Application.config.secret_token = '52f6e15703df377a2b0ad9bd0a17b0b43831a163004b6b43dae25e31abcbb6b058c7724c19600bf629ae40dda0504e61281efcaf611171ed7173b886c8b81c3a'
